9d16fdaffba7c5212d3f262329cbf795ddacbc7f
mohamdsy88 <mohamd15915915988@gmail.com>
2026-03-26 22:30:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// 🔧 تحميل الإعدادات (البوت + قاعدة البيانات)
 require('./config/config');
-
-// 🎯 نظام تسجيل اللاعبين
 require('./handlers/registerHandler');
-
-// 👑 لوحة الأدمن
 require('./adminHandlers/adminPanel');
+require('./agentHandlers/agentPanel');
 
-// 🚀 رسالة تشغيل
 console.log("🚀 SpinX Bot is running...");
