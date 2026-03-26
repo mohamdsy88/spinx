1e4072b8bae430cdb0b2326c092867994ad39c53
mohamdsy88 <mohamd15915915988@gmail.com>
2026-03-26 22:48:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ require('./config/config');
 require('./handlers/registerHandler');
 require('./adminHandlers/adminPanel');
 require('./agentHandlers/agentPanel');
+require('./playerHandlers/playerPanel');
 
 console.log("🚀 SpinX Bot is running...");
