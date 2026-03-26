133648dbea41cb37addda8db6a56e017e3be1917
mohamdsy88 <mohamd15915915988@gmail.com>
2026-03-26 04:56:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Bot is running...");
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, "البوت يعمل بنجاح 🚀");
+});
