c093670b1fd0a0f0125d128c64b9634ac66a377b
mohamdsy88 <mohamd15915915988@gmail.com>
2026-03-27 05:46:41 +0300

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,25 +1,28 @@
+// utils/helpers.js
+
 function generateUserId() {
   return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
+function generateRequestId() {
+  return 'REQ-' + Math.floor(100000 + Math.random() * 900000).toString();
+}
+
 function formatUSD(amount) {
-  const num = Number(amount) || 0;
-  return `${num.toFixed(2)} USD 💵`;
+  const num = Number(amount);
+  return `${num.toFixed(2)} دولار أمريكي 💵`;
 }
 
 function formatDate(date = new Date()) {
-  return date.toLocaleString('ar-SA', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
+  return date.toLocaleString('ar-EG', {
+    year:   'numeric',
+    month:  '2-digit',
+    day:    '2-digit',
+    hour:   '2-digit',
     minute: '2-digit',
-    hour12: true
+    hour12: false,
+    timeZone: 'Asia/Riyadh'
   });
 }
 
-function generateRequestId() {
-  return 'REQ' + Math.floor(100000 + Math.random() * 900000).toString();
-}
-
-module.exports = { generateUserId, formatUSD, formatDate, generateRequestId };
+module.exports = { generateUserId, generateRequestId, formatUSD, formatDate };
